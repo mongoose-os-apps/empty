ade66e68e32656dbc546583a9a92b6cd4163a99c
Deomid Ryabkov <rojer@cesanta.com>
2019-03-27 12:03:48 +0000

    RS14100: Flash and FS support, increase CPU and flash clock
    Run CPU at 180 MHz, flash at up to 100.
    
    CL: RS14100: FS support, increase CPU and flash clock
    
    PUBLISHED_FROM=e9f995bf03e4e36dc66e19a2a2d60ae1675ecaae

--- src/main.c
+++ src/main.c
@@ -17,7 +17,23 @@
 
 #include "mgos.h"
 
+static void timer_cb(void *arg) {
+  static bool s_tick_tock = false;
+  LOG(LL_INFO,
+      ("%s uptime: %.2lf, RAM: %lu, %lu free", (s_tick_tock ? "Tick" : "Tock"),
+       mgos_uptime(), (unsigned long) mgos_get_heap_size(),
+       (unsigned long) mgos_get_free_heap_size()));
+  s_tick_tock = !s_tick_tock;
+#ifdef LED_PIN
+  mgos_gpio_toggle(LED_PIN);
+#endif
+  (void) arg;
+}
+
 enum mgos_app_init_result mgos_app_init(void) {
-  LOG(LL_INFO, ("Hi there"));
+#ifdef LED_PIN
+  mgos_gpio_setup_output(LED_PIN, 0);
+#endif
+  mgos_set_timer(1000 /* ms */, MGOS_TIMER_REPEAT, timer_cb, NULL);
   return MGOS_APP_INIT_SUCCESS;
 }
